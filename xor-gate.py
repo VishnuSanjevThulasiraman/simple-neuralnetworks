4189fad3a75a9b0c0490d0ed1af591633ff859a6
VishnuSanjevThulasiraman <52427253+VishnuSanjevThulasiraman@users.noreply.github.com>
2020-04-27 15:19:37 +0530

    bias shouldn't have been a vector ; code works fine now ; created a new variable which takes the average of errors

--- xor-gate.py
+++ xor-gate.py
@@ -7,33 +7,37 @@ tr_input = np.array([[0,0],
                             [1,0],
                             [1,1]])
 
-output = np.array([[0,1,1,0]]).T
+output = np.array([[1,1,1,0]]).T
 
-np.random.seed(1)      #generating pseudo random numbers to debug easily
+np.random.seed(16)      #generating pseudo random numbers to debug easily
 
 w = 2 * np.random.random((2, 1)) - 1
-b = 2 * np.random.random((4, 1)) - 1
+b = 2 * np.random.random((1, 1)) - 1
 
 lst10 = []   #lists to generate matplot
 lst20 = []   #each list is to store
-lst30 = []   #the values of the changing_output
+lst30 = []   #the values of the c
 lst40 = []   #after each iteration
+lst_err = []
 
 for i in range(10000): #200 is the epochs you can change the values here to see how the
                      #accuracy of the network increases as you increase the value of epochs
 
     input = tr_input
-    changing_output = sigmoid.value(np.dot(input, w) + b )
+    c = sigmoid.value(np.dot(input, w) + b )
 
-    err = changing_output - output
-    change = err * sigmoid.dydx(changing_output)
+    err = c - output
+    change = err * sigmoid.dydx(c)
+    e = err[0]+ err[1] + err[2] + err[3]
+    e = e / 4
 
-    lst10.append(err[0])
-    lst20.append(err[1])
-    lst30.append(err[2])
-    lst40.append(err[3])
+    lst10.append(c[0])
+    lst20.append(c[1])
+    lst30.append(c[2])
+    lst40.append(c[3])
+    lst_err.append(e)
 
-    b -= 0.2 * err
+    b -= 0.2 * e
     w -= 0.2 * np.dot(input.T, change)
 
 print(' weights = ')
@@ -41,13 +45,14 @@ print(w)
 print(' bias = ')
 print(b)
 print(' output = ')
-print(changing_output)
+print(c)
 plt.plot(lst10)      #to view how the value changes over each iteration
 plt.plot(lst20)
 plt.plot(lst30)
 
 #print(change)
 plt.plot(lst40)
+plt.plot(lst_err)
 plt.title('Changing value of errors vs #iterations')
 #plt.savefig('fig1-tsv.png')
 plt.show()
