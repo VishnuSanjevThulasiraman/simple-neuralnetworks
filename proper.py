7ca47b858906f3ef3e38f55978494a22c167f158
VishnuSanjevThulasiraman <52427253+VishnuSanjevThulasiraman@users.noreply.github.com>
2020-05-13 03:01:41 +0530

    Added a new file proper.py

---
+++ proper.py
@@ -0,0 +1,59 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class sigmoid(object):
+
+    def value(x):                        #value of the sigmoid function
+
+        return 1 / (1+np.exp(-x))
+
+    def dydx(x):                        # derivative of the sigmoid function
+
+        return x * (1-x)
+
+np.random.seed(16)
+A = np.array([[0,0],[0,1],[1,1],[1,0]]).T
+B = np.array([1,1,0,1]).T
+
+np.random.seed(16)      #generating pseudo random numbers to debug easily
+
+W = 2 * np.random.random((2, 1)) - 1
+b = 2 * np.random.random((1, 1)) - 1
+c = []
+
+def cost(A,B,W,b):
+
+    C = sigmoid.value(np.dot(W.T,A))
+    E = C - B
+    SE = np.square(E)
+    MSE = 0.25 * np.sum(SE)
+
+    return(MSE)
+
+def backpropogation(A,B,W,b,epochs,lr): #lr is the learning rate
+
+    for i in range(epochs):
+
+        C = sigmoid.value(np.dot(W.T,A)+b)
+        E = C - B
+        sigmoid_derivatives = sigmoid.dydx(C)
+        Y = E * sigmoid_derivatives
+        YEt = np.dot(Y,A.T).T
+        W -= lr * YEt
+        b -= lr * np.sum(E)
+        #print(cost(A,B,W,b))
+        c.append(cost(A,B,W,b))
+
+
+    print(C)
+    plt.plot(c)
+
+backpropogation(A,B,W,b,10000,0.2)
+"""
+print(W.shape)
+print(W)
+print(A.shape)
+print(A)
+print(B.shape)
+print(B)
+"""
