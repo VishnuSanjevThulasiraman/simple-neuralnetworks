42f0f501b522581b34f3b8df431b305ea427d8c2
VishnuSanjevThulasiraman <52427253+VishnuSanjevThulasiraman@users.noreply.github.com>
2020-05-13 03:02:11 +0530

    minor changes ;

--- hidden-neurons.py
+++ hidden-neurons.py
@@ -16,16 +16,17 @@ def hidden_neuron(input, output, n_hn, epochs):
     bias1 = 2 * np.random.random((1, n_hn)) - 1
 
     weights2 = 2 * np.random.random((n_hn, 1)) - 1
-    bias2 = 2 * np.random.random((n_hn, 1)) - 1
+    bias2 = 2 * np.random.random((1 , 1)) - 1
 
     for i in range(epochs):
 
         hidden_neuron_output = sigmoid.value(np.dot(input, weights1) + bias1 )
         final_perceptron_output = sigmoid.value(np.dot(hidden_neuron_output, weights2) + bias2 )
         error = final_perceptron_output - output
+        e2 = (final_perceptron_output - output) * (final_perceptron_output - output)
         change = error * sigmoid.dydx(final_perceptron_output)
 
-        e = error[0] + error[1] + error[2] + error[3]
+        e = e2[0] + e2[1] + e2[2] + e2[3]
         e /= 4
 
         bias1 -= 0.35 * e
@@ -34,20 +35,24 @@ def hidden_neuron(input, output, n_hn, epochs):
         bias2 -= 0.35 * e
         weights2 -= 0.35 * np.dot(hidden_neuron_output.T, change)
 
-        lst100.append(hidden_neuron_output[0][0])
-        lst200.append(hidden_neuron_output[1][0])
-        lst300.append(hidden_neuron_output[2][0])
-        lst400.append(hidden_neuron_output[3][0])
-        lst_err0.append(e[0])
+        lst100.append(final_perceptron_output[0])
+        lst200.append(final_perceptron_output[1])
+        lst300.append(final_perceptron_output[2])
+        lst400.append(final_perceptron_output[3])
+        #lst_err0.append(e[0])
 
 
 
     print('mean error')
     print(e)
+    print('input =')
+    print(input)
     print('bias1')
     print(bias1)
     print('hidden_neuron_output')
     print(hidden_neuron_output)
+    print('final_perceptron_output')
+    print(final_perceptron_output)
     print('error')
     print(error)
     plt.plot(lst100)      #to view how the value changes over each iteration
@@ -56,7 +61,7 @@ def hidden_neuron(input, output, n_hn, epochs):
     plt.plot(lst400)
     plt.plot(lst_err0)
     plt.title('Changing value of errors vs #iterations')
-    #plt.show()
+    plt.show()
 
 tr_input = np.array([[0,0],
                     [0,1],
@@ -65,4 +70,4 @@ tr_input = np.array([[0,0],
 
 output = np.array([[0,0,0,1]]).T
 
-hidden_neuron(tr_input, output, 2, 100)
+hidden_neuron(tr_input, output, 2, 1000)
